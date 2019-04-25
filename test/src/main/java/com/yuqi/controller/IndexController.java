10059c78d59df7626d5d05d33ffdb0932a954107
mengship <dongchen0852@163.com>
2019-04-25 17:09:30 +0800

    mybatis例子

---
+++ test/src/main/java/com/yuqi/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.yuqi.controller;
+
+import com.yuqi.entity.User;
+import com.yuqi.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @ResponseBody
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/findByName")
+    public User findByName(String name){
+        return userMapper.findByName(name);
+    }
+
+    @ResponseBody
+    @RequestMapping("/insert")
+    public String insert(String name, Integer age){
+        userMapper.insert(name,age);
+        return "success";
+    }
+}
---
+++ test/src/main/java/com/yuqi/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.yuqi.mapper;
+
+import com.yuqi.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface UserMapper {
+	@Select("SELECT * FROM USERS WHERE NAME = #{name}")
+	User findByName(@Param("name") String name);
+	@Insert("INSERT INTO USERS(NAME, AGE) VALUES(#{name}, #{age})")
+	int insert(@Param("name") String name, @Param("age") Integer age);
+}
